de616d2cfbd9005137d1d657a51d1299fd9e9e17
Rikki407 <rishablamba407@gmail.com>
2018-06-18 00:49:25 +0530

    input.js modified

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Text, TextView, View} from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
-const Input = ({ label }) => {
+const Input = ({ label, value, onChangeText }) => {
     return (
         <View>
             <Text>{label}</Text>
+            <TextInput 
+                value={value}
+                onChangeText={onChangeText}
+                style={{ height: 20, width: 100 }} 
+            />
+
         </View>
     );
 };
