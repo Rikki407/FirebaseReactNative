090b1ceeef73b20ddb198012886fba9706ccdd63
Rikki407 <rishablamba407@gmail.com>
2018-06-17 18:45:28 +0530

    FireBase added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, {Component} from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common'
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyDeJW3JZ8uO9qXhmfRLMgpv0L5im2C8O1I",
+            authDomain: "fir-reactnative-8393d.firebaseapp.com",
+            databaseURL: "https://fir-reactnative-8393d.firebaseio.com",
+            projectId: "fir-reactnative-8393d",
+            storageBucket: "fir-reactnative-8393d.appspot.com",
+            messagingSenderId: "649849654853"
+          }
+        );
+    }
+
     render() {
         return(
             <View>
